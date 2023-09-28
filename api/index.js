e8a5c71f24ad18b4de5e46b9a44e4e3630611693
Pedro Perez <75536176+pperez303@users.noreply.github.com>
2023-09-28 09:00:39 -0700

    worked on ckeditor updates

--- api/index.js
+++ api/index.js
@@ -2,15 +2,16 @@ const express = require("express"); // same as the 'import Express from "express
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const multer = require("multer");
+const path = require("path");
+
+//Routes
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
-// test ckeditor
 const ckloadRoute = require("./routes/ckloads");
-// test ckeditor
 const categoryRoute = require("./routes/categories");
-const multer = require("multer");
-const path = require("path");
+
 
 dotenv.config();
 app.use(express.json());
